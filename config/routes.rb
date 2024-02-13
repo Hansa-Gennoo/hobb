5ac80c93616a68c38fd995f3b275b0af29397bfe
hansa gennoo <hansagennoo10@gmail.com>
2024-02-13 09:44:57 +0400

    HG created hobby controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: "hobbies#index"
-  resources :hobbies do
-    resources :events
-  end
+  resources :hobbies
   # Defines the root path route ("/")
   # root "posts#index"
 end
